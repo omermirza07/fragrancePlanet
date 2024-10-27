233851327be6ab0b070114c8a58660c54ede3859
shanthan.gunti <83639900+golden-requiem@users.noreply.github.com>
2024-10-26 21:28:44 -0400

    Update Header.js from newMain

--- fragrance-planet-frontend/src/components/Header.js
+++ fragrance-planet-frontend/src/components/Header.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
-  return (
-    <AppBar position="sticky" color="primary">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Fragrance Planet
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
-  );
+    const navigate = useNavigate();
+
+    return (
+        <AppBar position="sticky" color="primary">
+            <Toolbar>
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ flexGrow: 1, cursor: 'pointer' }}
+                    onClick={() => navigate('/')}
+                >
+                    Fragrance Planet
+                </Typography>
+                <Button color="inherit" onClick={() => navigate('/login')}>Login</Button>
+                <Button color="inherit" onClick={() => navigate('/signup')}>Sign Up</Button>
+                <Button color="inherit">About Us</Button>
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 export default Header;
